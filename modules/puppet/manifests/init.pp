5482f69bdb642aa5aa59d95bcea1c611ede75bd0
Dominik Förderreuther <vagrant@cookbook>
2014-02-27 17:00:42 +0000

    cron

--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -3,4 +3,16 @@ class puppet {
 		source => 'puppet:///modules/puppet/papply.sh', 
 		mode => '0755', 
 	}
+	
+	file { '/usr/local/bin/pull-updates': 
+		source => 'puppet:///modules/puppet/pull-updates.sh', 
+		mode => '0755', 
+	}
+	
+	cron { 'run-puppet': 
+		user => 'vagrant', 
+		command => '/usr/local/bin/pull-updates >> /var/log/puppet-pull-updates.log ', 
+		minute => '*/1', 
+		hour => '*', 
+	}
 }
