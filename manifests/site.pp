d97fee9f7e611902bb43efd37a2624a0648afe29
dfoerderreuther <root@cookbook>
2014-02-27 23:00:22 +0000

    java_home

--- manifests/site.pp
+++ manifests/site.pp
@@ -8,6 +8,22 @@ class toolbox {
         	content => "#!/bin/sh\npuppet agent --no-daemonize --onetime --verbose $1\n", 
         }
 }
+
+
+define appendLine($file, $line) {
+	exec { "/bin/echo '${line}' >> '${file}'":
+		unless => "/bin/grep -Fx '${line}' '${file}'",
+	}
+}	
+
+class java7withhome {
+	include java7
+	appendLine {
+		'java-home':
+		file => '/etc/environment',  
+		line => 'export JAVA_HOME=/usr/lib/jvm/java-7-oracle', 	
+	}
+}
  
 node 'puppetagent' {
         include toolbox
@@ -15,7 +31,6 @@ node 'puppetagent' {
 }
 node 'cookbook' {
 	include puppet
-	include java7
-	include tomcat7
+	include java7withhome
 	include mysite
 }
