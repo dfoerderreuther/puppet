4dd26aea061e8ab0c041266826b7a0928abdc12d
dfoerderreuther <dominik@eleon.de>
2014-03-05 20:33:23 +0000

    small changes

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,18 +1,3 @@
-/*
-TODO: Move
-*/
-class toolbox {
-        file {'/usr/local/sbin/puppetsimple.sh':
-	        owner => root, group => root, mode => 0755, 
-        	content => "#!/bin/sh\npuppet agent --no-daemonize --onetime --verbose $1\n", 
-        }
-}
-
-
- 
-node 'puppetagent' {
-        include toolbox
-}
 node 'myhost' {
 	include puppet
 	include java7, java7home
